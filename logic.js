8dc75656f49be148d7bbc721fdacc0b54997c0af
HiImCLoud <joshucalopez0107@gmail.com>
2025-02-08 16:34:35 +0800

    keys

--- logic.js
+++ logic.js
@@ -6,10 +6,10 @@ let columns = 4;
 
 function setGame() {
   board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [2, 0, 0, 0],
+    [2, 0, 0, 0],
+    [0, 4, 0, 0],
+    [0, 4, 0, 0],
   ];
 
   for (let r = 0; r < rows; r++) {
@@ -36,4 +36,51 @@ function updateTile(tile, num) {
   if (num > 0) {
     tile.innerText = num;
   }
+
+  if (num <= 4096) {
+    tile.classList.add("x" + num);
+  } else {
+    tile.classList.add("x8192");
+  }
+}
+
+window.onload -= function () {
+  setGame();
+};
+
+function handleSlide(event) {
+  if (
+    ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].includes(event.code)
+  ) {
+    if (event.code === "ArrowLeft") {
+      slideLeft();
+    } else if (event.code === "ArrowRight") {
+      slideRight();
+    } else if (event.code === "ArrowUp") {
+      slideUp();
+    } else if (event.code === "ArrowDown") {
+      slideDown();
+    }
+  }
+}
+document.addEventListener("keydown", handleSlide);
+
+function slideLeft() {
+  for (let r = 0; r < rows; r++) {
+    let row = board[r];
+  }
+
+  console.log("slideLeft");
+}
+
+function slideRight() {
+  console.log("slideRight");
+}
+
+function slideUp() {
+  console.log("slideUp");
+}
+
+function slideDown() {
+  console.log("slideDown");
 }
