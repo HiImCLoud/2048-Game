063fe47e7d4160c2812999a6b8c3ee2e2a6c11c5
HiImCLoud <joshucalopez0107@gmail.com>
2025-02-08 15:06:39 +0800

    JS logic

---
+++ logic.js
@@ -0,0 +1,23 @@
+let board;
+
+let score = 0;
+let rows = 4;
+let columns = 4;
+
+function setGame() {
+  board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement("div");
+      tile.id = r + "-" + c;
+    }
+  }
+}
+
+setGame();
